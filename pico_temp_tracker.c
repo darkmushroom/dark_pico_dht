bcdddbbbb1ab797f3075ffa4589f3f49baf7c084
darkmushroom <mensing.alexander@gmail.com>
2023-07-04 12:01:36 -0400

    refactor sensor read and data formatting into their own functions

--- pico_temp_tracker.c
+++ pico_temp_tracker.c
@@ -48,8 +48,13 @@
 #include "hardware/gpio.h"
 
 const uint DHT_PIN = 15;
+static uint byte_array[40];
+static int formatted_data[5];
+
 void request_reading();
 bool sensor_acknowledge();
+bool read_data();
+bool format_data();
 
 int main() {
     stdio_init_all();
@@ -60,72 +65,20 @@ int main() {
         if (sensor_acknowledge() == false) {
             printf("Sensor did not acknowledge read request.\n");
         }
-        else {
+    
+        sleep_us(4); // buffer between sensor ack and data
 
-            sleep_us(4); // buffer between sensor ack and data
-            /*
-                Let's read the sensor values. Better programmers
-                will stuff the bits into bytes as values are read,
-                but I will settle on an int array. This uses 16x
-                the memory (80bytes to store 40 bits).. but that's
-                about 0.003% of our available mem
-            */
-
-            uint byte_array[40];
-            uint last = 0;
-            uint j = 0;
-            while (j < 40) {
-                uint hi_count = 0;
-                for (uint kill = 0; kill < 256; kill++) {
-                    // we sleep
-                    if (gpio_get(DHT_PIN)) break;
-                    sleep_us(1);
-                }
-                while (gpio_get(DHT_PIN)) {
-                    // real shit
-                    hi_count++;
-                    sleep_us(1);
-                }
-
-                /*
-                    For some reason, the short pulse-width (0) is *very*
-                    stable and accurate, always coming in at a count of 20.
-                    For this reason, we'll count anything longer than 30
-                    as a high bit (1).
-                */
-                if (hi_count > 30) {
-                    byte_array[j] = 1;
-                }
-                else {
-                    byte_array[j] = 0;
-                }
-
-                j++;
-                sleep_us(1);
-            }
-
-            // FIXME: Does not work for negative temp values... which is sort of the point
-            int formatted_data[5];
-            uint k = 4;
-            int builder = 0;
-            uint power = 1;
-            for (uint i = 0; i < 40; i++) {
-                builder += byte_array[(40 - 1) - i] * power;
-                power *= 2;
-                if ((i+1) % 8 == 0 && i != 40) {
-                    formatted_data[k] = builder;
-                    k--;
-                    builder = 0;
-                    power = 1;
-                }
-            }
-
-            printf("Actual reading: ");
-            float humidity = ((256 * ((float)formatted_data[0] + ((float)formatted_data[1]/256)))/10);
-            float temp = ((256 * ((float)formatted_data[2] + ((float)formatted_data[3]/256)))/10);
-            printf("Humidity: %.1f%% || ", humidity);
-            printf("Temperature: %.1fC\n", temp);
+        if (read_data() == false) {
+            printf("Did not receive a full 40bits of data.\n");
         }
+
+        format_data();
+
+        printf("Actual reading: ");
+        float humidity = ((256 * ((float)formatted_data[0] + ((float)formatted_data[1]/256)))/10);
+        float temp = ((256 * ((float)formatted_data[2] + ((float)formatted_data[3]/256)))/10);
+        printf("Humidity: %.1f%% || ", humidity);
+        printf("Temperature: %.1fC\n", temp);
 
         sleep_ms(2000);
     }
@@ -159,7 +112,6 @@ bool sensor_acknowledge() {
     bool ack_part_2 = false;
     uint count = 0;
     
-    // TODO: Part 1 of the acknowledgement can probably be skipped, line high is more interesting
     for (uint frame = 0; frame < 255; frame++) {
         if (gpio_get(DHT_PIN) == 0) count++;
         if (count > 65) {
@@ -181,4 +133,71 @@ bool sensor_acknowledge() {
     }
 
     return ack_part_1 && ack_part_2;
+}
+
+/*
+    Let's read the sensor values. Better programmers
+    will stuff the bits into bytes as values are read, but
+    I will settle on a static int array. This uses 16x the
+    memory (80bytes to store 40 bits).. but that's about
+    0.003% of our available mem
+*/
+bool read_data() {
+
+    uint last = 0;
+    uint j = 0;
+    while (j < 40) {
+        uint hi_count = 0;
+        for (uint kill = 0; kill < 256; kill++) {
+            // we sleep
+            if (gpio_get(DHT_PIN)) break;
+            sleep_us(1);
+        }
+        while (gpio_get(DHT_PIN)) {
+            // real shit
+            hi_count++;
+            sleep_us(1);
+        }
+
+        /*
+            For some reason, the short pulse-width (0) is *very*
+            stable and accurate, always coming in at a count of 20.
+            For this reason, we'll count anything longer than 30
+            as a high bit (1).
+        */
+        if (hi_count > 30) {
+            byte_array[j] = 1;
+        }
+        else {
+            byte_array[j] = 0;
+        }
+
+        j++;
+    }
+
+    return j == 40;
+}
+
+/*
+    Turn our big byte_array into a more manageable int
+    array. There are fewer places in the code it is more
+    clear I am uncomfortable with bitwise operations.
+*/
+bool format_data() {
+    // FIXME: Does not work for negative temp values... which is sort of the point
+    uint k = 4;
+    int accumulator = 0;
+    uint power = 1;
+    for (uint i = 0; i < 40; i++) {
+        accumulator += byte_array[(40 - 1) - i] * power;
+        power *= 2;
+        if ((i+1) % 8 == 0 && i != 40) {
+            formatted_data[k] = accumulator;
+            k--;
+            accumulator = 0;
+            power = 1;
+        }
+    }
+    
+    return formatted_data[0] + formatted_data[1] + formatted_data[2] + formatted_data[3] == formatted_data[4];
 }
